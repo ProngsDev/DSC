310540d9828ea9e088cddd6abf686797c34ee420
Prongs-Dev <175581281+ProngsDev@users.noreply.github.com>
2025-05-28 03:26:07 +0700

    feat: initilize the DSC.

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin_NotZeroAddress();
+    error DecentralizedStableCoin_AmountMustBeGreaterThanZero();
+    error DecentralizedStableCoin_BurnAmountExceedsBalance();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) revert DecentralizedStableCoin_NotZeroAddress();
+        if (_amount <= 0)
+            revert DecentralizedStableCoin_AmountMustBeGreaterThanZero();
+        _mint(_to, _amount);
+        return true;
+    }
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount < 0)
+            revert DecentralizedStableCoin_AmountMustBeGreaterThanZero();
+        if (_amount > balance)
+            revert DecentralizedStableCoin_BurnAmountExceedsBalance();
+        super.burn(_amount);
+    }
+}
