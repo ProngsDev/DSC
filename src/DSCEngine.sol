6db404eb33838b1b7f869f6bba506c6da490203a
Prongs-Dev <175581281+ProngsDev@users.noreply.github.com>
2025-05-29 15:02:13 +0700

    feat: add collateral management.

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -2,12 +2,19 @@
 pragma solidity ^0.8.20;
 
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 
 contract DSCEngine is ReentrancyGuard {
     //errors
 
     error DSCEngine_TokenAddressAndPriceFeedAddressesAmountsDontMatch();
+    error DSCEngine_NeedMoreThanZero();
+    error DSCEngine_TokenNotSupported(address token);
+    error DSCEngine_TransferFailed();
+    error DSCEngine_InsufficientCollateral();
+    error DSCEngine_RedeemFailed();
 
     //types
 
@@ -17,8 +24,9 @@ contract DSCEngine is ReentrancyGuard {
     address[] public collateralTokens;
 
     mapping(address => address) public priceFeeds;
-
     mapping(address => address) public collateralBalance;
+
+    mapping(address => mapping(address => uint256)) public userCollateralBalance;
 
     constructor(address[] memory _collateralTokens, address[] memory _priceFeeds, address dscAddress) {
         if (_collateralTokens.length != _priceFeeds.length) {
@@ -30,5 +38,29 @@ contract DSCEngine is ReentrancyGuard {
             collateralTokens.push(_collateralTokens[i]);
         }
         i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    function depositCollateral(address token, uint256 amount) external nonReentrant {
+        if (amount == 0) revert DSCEngine_NeedMoreThanZero();
+        if (priceFeeds[token] == address(0)) {
+            revert DSCEngine_TokenNotSupported(token);
+        }
+
+        userCollateralBalance[msg.sender][token] += amount;
+
+        bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
+        if (!success) revert DSCEngine_TransferFailed();
+        //Future: emit event
+    }
+
+    function redeemCollateral(address token, uint256 amount) external nonReentrant {
+        if (amount == 0) revert DSCEngine_NeedMoreThanZero();
+        uint256 userBalance = userCollateralBalance[msg.sender][token];
+        if (userBalance < amount) revert DSCEngine_InsufficientCollateral();
+
+        userCollateralBalance[msg.sender][token] -= amount;
+
+        bool success = IERC20(token).transfer(msg.sender, amount);
+        if (!success) revert DSCEngine_RedeemFailed();
     }
 }
